31826d78dbe59cb5c27e9b0d000a616c022b60d6
mrFrok <mrFrok228@proton.me>
2026-06-30 15:04:22 +0300

    edit forms.py

--- landing/forms.py
+++ landing/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from .models import HeroBlock
+from django.forms import inlineformset_factory
+from .models import HeroBlock, HeroStat
 
 
 class HeroForm(forms.ModelForm):
     class Meta:
         model = HeroBlock
         fields = ["title", "subtitle"]
+    HeroStatFromSet = inlineformset_factory(
+        HeroBlock,
+        HeroStat,
+        fields=["title", "subtitile", "order"],
+        extra=1,
+        can_delete=True,
+    )
